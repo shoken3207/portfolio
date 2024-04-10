36be08fab4793db1fd1e68ac69b08d96275c7723
KCSNET\st20214101 <shoken4797@gmail.com>
2024-04-10 13:22:43 +0900

    IntersectionObserverでスクロールに連動したアニメーション実装

--- src/components/work/SectionTemplate.tsx
+++ src/components/work/SectionTemplate.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+import React, { useRef } from "react";
+import IntersectionObserverWrap from "./IntersectionObserverWrap";
 
 const SectionTemplate = ({
   title,
@@ -7,13 +9,17 @@ const SectionTemplate = ({
   title: string;
   children: React.ReactNode;
 }) => {
+ 
   return (
+    <IntersectionObserverWrap>
+
     <div className="flex flex-col gap-2">
       <h2 className="text-3xl font-bold pl-2 border-l-8 border-blue-600">
         {title}
       </h2>
       {children}
     </div>
+    </IntersectionObserverWrap>
   );
 };
 
---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,22 @@
+import {RefObject, useEffect} from "react"
+
+export const useIntersectionObserver = (refs: RefObject<HTMLElement>[], callback: (entries: IntersectionObserverEntry[])=> void,options?: IntersectionObserverInit):void => {
+    useEffect(() => {
+        const observer = new IntersectionObserver(callback, options)
+
+        refs.forEach(ref => {
+            if(ref.current) {
+                observer.observe(ref.current)
+            }
+        })
+        return () => {
+            refs.forEach(ref => {
+                if(ref.current) {
+                    observer.unobserve(ref.current)
+                }
+            })
+        }
+    })
+
+
+}
