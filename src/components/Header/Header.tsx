84a99c65cd1ab3d2e83609dcee3ac5e5fe82dbb0
shoken <shoken4797@gmail.com>
2024-04-11 03:24:24 +0900

    ヘッダー修正

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,34 +4,25 @@ import NavigationComponent from "../NavigationComponent";
 import LinkWrap from "../common/LinkWrap";
 
 const Header = () => {
-  const headerRef = useRef<HTMLHeadingElement>(null);
-  const [headerHeight, setHeaderHeight] = useState<number | undefined>(0);
-  let headerClass =
-    "z-10 fixed w-full mb-6 bg-white p-2 border-t-4 border-blue-400 shadow-md";
-  useEffect(() => {
-    setHeaderHeight(headerRef.current?.clientHeight);
-    // console.log("headerHeight: ", headerRef.current?.clientHeight);
-  }, []);
   return (
-    <header
-      ref={headerRef}
-      className={`${
-        !headerHeight && `mb-[${headerHeight}px]`
-      } z-10 fixed w-full mb-6 bg-white p-2 border-t-4 border-blue-400 shadow-md`}
-    >
-      <div className=" 1/12 mx-auto max-w-3xl flex justify-between items-center transition-opacity">
-        <LinkWrap href="/page2">
-          <h1 className="duration-300 text-4xl cursor-pointer hover:opacity-50 transition-duration: 150ms">
-            <img
-              className="w-16"
-              src="/images/f_f_business_30_s512_f_business_30_0nbg.png"
-              alt=""
-            />
-          </h1>
-        </LinkWrap>
-        <NavigationComponent />
-      </div>
-    </header>
+    <div className="pb-[80px]">
+      <header
+        className={`fixed top-0 z-10 w-full bg-white p-2 border-t-4 border-blue-400 shadow-md`}
+      >
+        <div className=" w-10/12 mx-auto max-w-3xl flex justify-between items-center transition-opacity">
+          <LinkWrap href="/page2">
+            <h1 className="duration-300 text-4xl cursor-pointer hover:opacity-50 transition-duration: 150ms">
+              <img
+                className="w-16"
+                src="/images/f_f_business_30_s512_f_business_30_0nbg.png"
+                alt=""
+              />
+            </h1>
+          </LinkWrap>
+          <NavigationComponent />
+        </div>
+      </header>
+    </div>
   );
 };
 
