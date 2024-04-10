c5d08847885796a105260a88dc8fcb5d513528e9
shoken <shoken4797@gmail.com>
2024-04-11 03:31:52 +0900

    ヘッダー修正

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,14 +9,17 @@ const Header = () => {
       <header
         className={`fixed top-0 z-10 w-full bg-white p-2 border-t-4 border-blue-400 shadow-md`}
       >
-        <div className=" w-10/12 mx-auto max-w-3xl flex justify-between items-center transition-opacity">
+        <div className=" w-11/12 mx-auto max-w-3xl flex justify-between items-center transition-opacity">
           <LinkWrap href="/page2">
-            <h1 className="duration-300 text-4xl cursor-pointer hover:opacity-50 transition-duration: 150ms">
+            <h1 className="duration-300 cursor-pointer hover:opacity-50 transition-duration: 150ms flex items-center gap-x-1.5">
               <img
-                className="w-16"
+                className="w-8"
                 src="/images/f_f_business_30_s512_f_business_30_0nbg.png"
                 alt=""
               />
+              <span className=" text-2xl font-bold text-gray-500">
+                Portfolio
+              </span>
             </h1>
           </LinkWrap>
           <NavigationComponent />
