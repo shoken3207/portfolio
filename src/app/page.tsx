5e1e1aec39270ee88be1ec3ea1ac088e293aecb6
ezaki <shoken4797@gmail.com>
2024-05-01 12:12:00 +0900

    ondemand isr実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,4 @@
-import Work from "@/components/Work";
 import Layout from "@/components/common/Layout";
-import Section from "@/components/common/Section";
-import Form from "@/components/form/Form";
-import History from "@/components/history/History";
-import Home from "@/components/home/Home";
-import MotionWrapper from "@/components/motionWrapper/motionWrapper";
-import Profile from "@/components/profile/Profile";
-import Skill from "@/components/skill/Skill";
 import { getPortfolioInfo } from "@/libs/microcms";
 import { portfolio } from "@/types/cms-portfolio-types";
 import React from "react";
