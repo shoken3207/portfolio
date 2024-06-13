60922499822e727e8b0ed5887761c8876757e189
ezaki <shoken4797@gmail.com>
2024-06-13 13:51:24 +0900

    3dモデル追加

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,34 @@
+"use client";
+
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+
+const Model = () => {
+  const { scene } = useGLTF("/models/shiba/shiba.gltf");
+  return <primitive object={scene} />;
+};
+
+const Scene = () => {
+  return (
+    <>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <OrbitControls />
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+    </>
+  );
+};
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Canvas style={{ width: "100vw", height: "100vh" }}>
+        <Scene />
+      </Canvas>
+    </div>
+  );
 };
 
 export default Home;
