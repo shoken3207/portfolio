114cc4e6d70d410f9b9acf06f5c28e59fdc9da2f
shoken <shoken4797@gmail.com>
2024-06-27 13:27:40 +0900

    buildエラー対応

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const BackButton = ({ workId }: { workId?: string }) => {
   const router = useRouter();
+  const [hasPrevPage, setHasPrevPage] = useState<Boolean>(true);
+  useEffect(() => {
+    setHasPrevPage(window.history.length > 1);
+  }, []);
   return (
     <>
-      {window.history.length > 1 ? (
+      {hasPrevPage ? (
         <img
           className="w-6 font-bold cursor-pointer sm:w-8 lg:w-10"
           src="/icons/back-svgrepo-com.svg"
